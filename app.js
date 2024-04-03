b8570960728edd68c2b290c6d30964d7501fc94e
udara <udaraprabath98@gmail.com>
2024-04-03 21:58:11 +0530

    first-commit

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+var corsOptions = {
+    origin: 'http://localhost:8080',
+    optionsSuccessStatus: 200 ,
+    methods: "GET"
+}
+app.use(cors(corsOptions))
+ globalObj = {}
+ var districts = ['Colombo','Kandy','Kalutara','Matale','Gampaha','Nuwara Eliya','Galle','Matara','Habmanthota','jaffna','Kilinochchi','Manner','Vavuniya','Mullaitivu','Batticaloa','Ampara','Trincomalee','Kurunagala','puththalam','Anuradhapura','Polonnaruwa','Badulla','Monaragala','Rathnapura','Kegalle']
+
+const maxTemperature = 40.00; 
+const minTemperature = -10.00;
+
+const minHumidity = 0.00; 
+const maxHumidity = 1.00;
+
+const minAirPressure = 900.00; 
+const maxAirPressure = 1100.00;
+
+
+function randomVal(min,max)
+{
+  return (Math.random()*(max-min)+min).toFixed(2);
+}
+
+function createValues()
+{
+ globalObj = districts.map((district)=>{
+   return {
+     district:district,
+     temperature: randomVal(minTemperature,maxTemperature),
+     humidity: randomVal(minHumidity,maxHumidity),
+     airPressure: randomVal(minAirPressure,maxAirPressure)
+   }
+  })  
+}
+
+
+createValues()
+setInterval(createValues, 5000*60);
+
+app.get('/', (req, res) => {
+    res.json({
+     globalObj
+    });
+});
+app.listen(3000)
