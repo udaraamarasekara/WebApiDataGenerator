253dce5dbbf418b6ec5e8489198410158d413432
udara <udaraprabath98@gmail.com>
2024-04-04 21:07:04 +0530

    before host-part-fourttt

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
-var corsOptions = {
-    origin: ' https://backend-web-api-75f83f1efeee.herokuapp.com/ ',
-    optionsSuccessStatus: 200 ,
-    methods: "GET"
-}
-app.use(cors(corsOptions))
+const basicAuth = require('basic-auth')
+
  globalObj = {}
  var districts = ['Colombo','Kandy','Kalutara','Matale','Gampaha','Nuwara Eliya','Galle','Matara','Habmanthota','jaffna','Kilinochchi','Manner','Vavuniya','Mullaitivu','Batticaloa','Ampara','Trincomalee','Kurunagala','puththalam','Anuradhapura','Polonnaruwa','Badulla','Monaragala','Rathnapura','Kegalle']
 
@@ -19,7 +14,19 @@ const maxHumidity = 1.00;
 const minAirPressure = 900.00; 
 const maxAirPressure = 1100.00;
 
+const USERNAME = "backenduser";
+const PASSWORD = "backendpass";
+const auth = (req, res, next) => {
+  const credentials = basicAuth(req);
+   console.log(credentials)
+   if (!credentials || credentials.name !== USERNAME || credentials.pass !== PASSWORD) {
+    res.set('WWW-Authenticate', 'Basic realm="Authorization Required"');
+      return res.status(401).send('Unauthorized');
+  }
 
+  return next();
+};
+app.use(auth);
 function randomVal(min,max)
 {
   return (Math.random()*(max-min)+min).toFixed(2);
@@ -41,9 +48,10 @@ function createValues()
 createValues()
 setInterval(createValues, 5000*60);
 
-app.get('/',cors(), (req, res) => {
+app.get('/', (req, res) => {
     res.json({
      globalObj
     });
 });
-app.listen(process.env.PORT || 3000)
+//app.listen(3000)
+ app.listen(process.env.PORT || 3000)
